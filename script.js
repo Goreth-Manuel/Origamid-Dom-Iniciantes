3c3fcefc8468f39c56763587228e1b40a1f3b8d3
Goreth Manuel <gorethmanuel0@gmail.com>
2024-09-17 20:19:08 +0100

    feat(MAM-03) Adiciona navegação por Tab :rocket:

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-// const animais = document.getElementById('animais');
-// console.log(animais);
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-// const gridSection = document.getElementsByClassName('grid-section');
-// console.log(gridSection[0]);
+function activeTab(index) {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    })
+    tabContent[index].classList.add('ativo');
+}
 
-// const primeiraLi = document.querySelector('li')
-// console.log(primeiraLi);
-
-// const primeiraUl = document.querySelector('ul');
-// console.log(primeiraUl);
-
-// const linkInterno = document.querySelector('[href^="#"]');
-// console.log(linkInterno);
-
-// const animaisImg = document.querySelectorAll('.animais img');
-// console.log(animaisImg)
-
-// Retorna no console todas as imagens do site.
-const allImg = document.querySelectorAll('img');
-console.log(allImg);
-
-// Retorne no console apenas as imagens que começaram com a palavra imagem
-const imgensAnimais = document.querySelectorAll('img[src^="img/imagem"]')
-console.log(imgensAnimais);
-
-// Seleciona todos os links internos onde o href começa com #
-const linksInternos = document.querySelectorAll('[href^="#"]');
-console.log(linksInternos);
-
-// Seleciona o primeiro h2 dentro de .animais-descricao
-const h2 = document.querySelector('.animais-descricao h2');
-console.log(h2)
-
-// Selecione o último p do site
-const paragrafo = document.querySelectorAll('P')
-console.log(paragrafo[paragrafo.length - 1])
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index)
+    } )
+})
