dae4ac20e38253b69da8f02d7889cbfc563d7047
Goreth Manuel <gorethmanuel0@gmail.com>
2024-09-18 22:26:57 +0100

    feat(MAM-03) Adiciona a função de seleção dos dt e dl :rocket:

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-
-if(tabMenu.length && tabContent.length){
-    tabContent[0].classList.add('ativo');
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
     
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add('ativo');
+        
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            })
+            tabContent[index].classList.add('ativo');
+        }
+        
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index)
+            } )
         })
-        tabContent[index].classList.add('ativo');
-    }
     
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index)
-        } )
-    })
+    }
 
 }
+initTabNav()
+
+const accordionList = document.querySelector('.js-accordion dt');
+
+function activeAccordion(){
+    this.classList.add('activo');
+    this.nextElementSibling.classList.add('activo');
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion)
+})
