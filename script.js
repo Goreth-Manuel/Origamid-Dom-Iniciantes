bdf390a36934d6fd8f2cfb43b7038c1731532a22
Goreth Manuel <gorethmanuel0@gmail.com>
2024-09-19 22:37:25 +0100

    feat(MAM-04) Finiliza a criação da função initScrollSuave :rocket:

--- script.js
+++ script.js
@@ -44,15 +44,22 @@ function initAccordion() {
 }
 initAccordion();
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
-
-function scrollToSection (event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href)
-
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    
+    function scrollToSection (event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+    
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
+    
+    linksInternos.forEach((item) => {
+        item.addEventListener('click', scrollToSection);
+    } )
 }
-
-linksInternos.forEach((item) => {
-    item.addEventListener('click', scrollToSection);
-} )
+initScrollSuave();
