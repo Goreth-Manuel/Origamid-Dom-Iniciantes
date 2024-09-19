6974e7c62570f5ab9312adb581dbb3329e4f5338
Goreth Manuel <gorethmanuel0@gmail.com>
2024-09-19 21:14:43 +0100

    feat(MAM-04) Finaliza a função initAccordion :rocket:

--- script.js
+++ script.js
@@ -23,18 +23,23 @@ function initTabNav() {
 }
 initTabNav()
 
+
 function initAccordion() {
     const accordionList = document.querySelectorAll('.js-accordion dt');
-    accordionList[0].classList.add('ativo');
-    accordionList[0].nextElementSibling.classList.add('ativo');
-    
-    function activeAccordion() {
-        this.classList.toggle('ativo');
-        this.nextElementSibling.classList.toggle('ativo');
+    const activeClass = 'ativo'
+
+    if(accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+        
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+        
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion)
+        })
     }
-    
-    accordionList.forEach((item) => {
-        item.addEventListener('click', activeAccordion)
-    })
 }
 initAccordion()
